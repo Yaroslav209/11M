d40dee11d15c642797a18a01403a4cdcfe37e0e4
Yaroslav209 <yaroslavsapegin@gmail.com>
2018-01-26 16:31:32 +0300

    Update and rename .gitignore to существование эйлерова цикла.cpp

---
+++ .gitignore/существование эйлерова цикла.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int v, r;
+	cin >> v >> r;
+	bool Ep = 1;
+vector <int> G(v, v);
+for(int i = 0; i < v; i++){
+    for(int j = 0; j < v; j++)
+G[i][i] = 0;
+}
+for(int i = 0; i < r; i++){
+	int m, n;
+	cin >> m >> n;
+	G[m][n] = 1;
+	G[n][m] = 1;
+}
+// cчитывание графа с помощью матрицы смежности
+for(int i = 0; i < v; i++){
+	int c = 0;
+	// с - степень вершины
+	for(int j = 0; j < r; j++)
+		c = c + G[i][j];
+		int a = c / 2;
+		if(a + a != c)
+		Ep = 0;
+}
+// проверка вершин на чётность степени
+if (Ep = 0) 
+cout << "NONE";
+else cout << "Yes";
+	return 0;
+}
